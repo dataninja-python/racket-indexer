1c35ab105a6931327ae04298074d51649476c9e5
dataninja-python <the.coders.dao@gmail.com>
2023-11-16 18:52:12 -0500

    3rd attempt to fix display to console issue

--- main.rkt
+++ main.rkt
@@ -1,14 +1,13 @@
 #lang racket
 
-
 (define (play-game)
     ;; this function does x
-    displayln "Hi from the play-game function"
+    (displayln "Hi from the play-game function")
 )
 
 (define (init-game)
     ;; this function does x
-    displayln "Hi from the init-game function"
+    (displayln "Hi from the init-game function")
 )
 
 ;; @problem:
