1a3ab4151a90206afddd3264c540c1825b720f81
dataninja-python <the.coders.dao@gmail.com>
2023-11-16 15:39:39 -0500

    rewrote main.rkt to be more idiomatic. TODO: add comments, etc

--- main.rkt
+++ main.rkt
@@ -1,65 +1,17 @@
 #lang racket
 
+;; requirements to run the program
 (require racket/cmdline
          racket/format
          racket/path
          racket/list)
 
-;; set global variables
-(define turns 3)
-(define minimum-number 1)
-(define maximum-number 100)
-(define secret-number 50)
-(define guess -1)
-(define message "Hello World")
-(define used-turns 0)
-
-;; set global helper functions
-
-;; get-user-input gets user input
-(define (get-user-input msg)
-    (begin
-        (display msg) ;; display a prompt
-        ;;(flush-output-port) ;; display the propt immediately
-        (let ((user-input (read-line)))
-            user-input)))
-
-;; run-guess-game runs the guessing game
-(define (run-guessing-game)
-  (begin
-    (displayln "Welcome to the guessing game!")
-    (displayln (format "I'm thinking of a number between ~a and ~a." minimum-number maximum-number))
-    (displayln "Can you guess it?")
-    (displayln (format "You have ~a turns to guess." turns))
-    (guessing-game)))
-
-;; guessing-game implements the guessing game logic
-(define (guessing-game)
-    (set! guess (get-guess)) ;; get a users guess
-    (set! used-turns (+ used-turns 1)) ;; inrement the number of used turns
-    (cond
-        [(> used-turns turns) (displayln "You lose!")]
-        [(= used-turns turns) (displayln "Last turn!")] ;; last turn
-        [else (displayln "play game!")])
-    (displayln (format "You have ~a turns left." turns)
-    (guessing-game turns)))
-
-;; get-guess gets a guess from the user
-(define (get-guess)
-    (get-user-input "What is your guess? "))
-
-;; display-turns shows user the number of guesses remaining
-(define (display-turns N)
-    (displayln (format "You have ~a turns left." N)))
-
-;; main function runs the program
-(define (main)
-  (run-guessing-game))
-
-(main)
-
-#lang racket
-
+;; @problem:
+;; @solution:
+;; @input:
+;; @output:
+;; @details:
+;; run-guessing-game runs the guessing game
 (define (run-guessing-game)
   (define max-turns 3)
   (define min-number 1)
