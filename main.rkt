510a6c4dbb3b9c7615b04fb5ef808e6c1d585153
dataninja-python <the.coders.dao@gmail.com>
2023-11-16 15:35:43 -0500

    inserted code from terminal testing into project code

--- main.rkt
+++ main.rkt
@@ -57,3 +57,47 @@
   (run-guessing-game))
 
 (main)
+
+#lang racket
+
+(define (run-guessing-game)
+  (define max-turns 3)
+  (define min-number 1)
+  (define max-number 100)
+  (define secret-number (random (+ max-number 1)))
+  (define used-turns 0)
+
+  (displayln "Welcome to the guessing game!")
+  (displayln (format "I'm thinking of a number between ~a and ~a." min-number max-number))
+  (displayln "Can you guess it?")
+
+  (let loop ((remaining-turns max-turns))
+    (cond
+      [(= used-turns max-turns) (displayln "Last turn!")]
+      [(= used-turns (add1 max-turns)) (displayln "You lose!")]
+      [else (display-turns remaining-turns)
+            (let* ((guess (get-guess))
+                   (used-turns (add1 used-turns)))
+              (cond
+                [(= guess secret-number) (displayln "Congratulations! You guessed it!")
+                 (displayln (format "It took you ~a turns." used-turns))]
+                [(> guess secret-number) (displayln "Too high!")]
+                [else (displayln "Too low!")])
+              (loop (sub1 remaining-turns)))])))
+
+(define (get-guess)
+  (get-user-input "What is your guess? "))
+
+(define (get-user-input msg)
+  (begin
+    (display msg)
+    (let ((user-input (read-line)))
+      user-input)))
+
+(define (display-turns remaining-turns)
+  (displayln (format "You have ~a turns left." remaining-turns)))
+
+(define (main)
+  (run-guessing-game))
+
+(main)
