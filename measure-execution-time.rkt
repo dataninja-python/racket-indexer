d8ccdef411b5f89421dbb35ebdaa9f0118586b4c
dataninja-python <the.coders.dao@gmail.com>
2023-11-18 11:54:11 -0500

    added function to measure execution time of a function

---
+++ measure-execution-time.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require racket/runtime)
+
+(define (measure-execution-time function-to-measure . args)
+  (let* ((start-time (current-inexact-milliseconds))
+         (result (apply function-to-measure args))
+         (end-time (current-inexact-milliseconds))
+         (elapsed-time (- end-time start-time)))
+    (values elapsed-time result)))
+
+(define (example-function a b)
+  ; Replace this with the code of the function you want to measure.
+  (let loop ((i 0) (result 0))
+    (if (< i a)
+        (loop (+ i 1) (+ result b))
+        result)))
+
+(define-values (elapsed-time result) (measure-execution-time example-function 1000000 2))
+
+(displayln (format "Execution time: ~a milliseconds" elapsed-time))
+(displayln (format "Result: ~a" result))
